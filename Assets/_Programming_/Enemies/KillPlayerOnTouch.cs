7d9575e35947e45420bd35d3116113342282fb51
Isaac <Isaacboon@gmail.com>
2019-07-04 12:11:31 +1200

    PlayerV4 working

--- Assets/_Programming_/Enemies/KillPlayerOnTouch.cs
+++ Assets/_Programming_/Enemies/KillPlayerOnTouch.cs
@@ -6,10 +6,20 @@ public class KillPlayerOnTouch : MonoBehaviour {
 
     private void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.tag == "Player" && PlayerManager.alive)
+        if (other.gameObject.tag == "Player" && PlayerManager.alive)
         {
             Debug.Log("KILL PLAYER");
             PlayerManager.playerDeath.killPlayer();
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && PlayerManager.alive)
+        {
+            Debug.Log("KILL PLAYER");
+            PlayerManager.playerDeath.killPlayer();
+        }
+    }
+
 }
